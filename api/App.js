53a96d415670ed01722a64670028c96b82c8303e
Shahid Khan <shahidkhan.prog@gmail.com>
2026-03-10 20:53:32 +0500

    working on prisma

--- api/App.js
+++ api/App.js
@@ -4,6 +4,7 @@ import authRoute from './routes/auth.route.js';
 const app = express();
 const PORT = 3000;
 
+// Middleware to parse JSON bodies
 app.use(express.json());
 
 app.use('/api/auth', authRoute);
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,13 @@
-export const register = (req, res) => {
-    //db
-    console.log("Register endpoint");
+import bcrypt from 'bcrypt';
+
+export const register = async (req, res) => {
+    
+    const { username, email, password } = req.body;
+    
+    // Hash password | bcrypt.hashSync return promise and we need to await it
+    const hashedPassword = await bcrypt.hashSync(password, 10);
+    
+    // Save user to database
 };
 
 export const login = (req, res) => {
