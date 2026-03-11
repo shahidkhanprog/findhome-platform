917bbeb23647d19348134e12d6a472a3f711a3a0
Shahid Khan <shahidkhan.prog@gmail.com>
2026-03-11 14:09:08 +0500

    new updates

--- api/App.js
+++ api/App.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from 'express';
 import authRoute from './routes/auth.route.js';
 
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,29 @@
 import bcrypt from 'bcrypt';
+import prisma from '../lib/prisma.js';
 
 export const register = async (req, res) => {
     
     const { username, email, password } = req.body;
     
-    // Hash password | bcrypt.hashSync return promise and we need to await it
+    try {
+        // Hash password | bcrypt.hashSync return promise and we need to await it
     const hashedPassword = await bcrypt.hashSync(password, 10);
-    
+
     // Save user to database
+    const newUser = await prisma.user.create({ 
+        data:{
+            username,
+            email,
+            password:hashedPassword,
+        }});
+
+    console.log("User registered:", newUser);
+
+    res.status(201).json({ message: "User registered successfully", user: newUser.username });
+    } catch (error) {
+        console.error("Error registering user:", error);
+        res.status(500).json({ message: "Error registering user", error: error.message });
+    }
 };
 
 export const login = (req, res) => {
